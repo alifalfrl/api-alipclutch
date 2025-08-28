992eda04c4731996b260f18f15bb217f3b466652
alifalfrl <alifalfarel10@gmail.com>
2025-08-28 07:25:49 +0700

    Update brat.js

--- api/brat.js
+++ api/brat.js
@@ -5,14 +5,14 @@ module.exports = {
   path: "/imagecreator/brat?apikey=&text=",
   async run(req, res) {
     const { apikey, text } = req.query;
-    if (!global.apikey.includes(apikey)) 
+    if (!global.apikey.includes(apikey))
       return res.json({ status: false, error: 'Apikey invalid' });
-    if (!text) 
+    if (!text)
       return res.json({ status: false, error: 'Missing text' });
 
     const encodedText = encodeURIComponent(text);
     const buffer = await getBuffer(
-      `https://brat.siputzx.my.id/image?text=${encodedText}&background=%23ffffff&color=%23000000&emojiStyle=apple`
+      `https://api.zenzxz.my.id/maker/brat?text=${encodedText}`
     );
 
     res.writeHead(200, {
