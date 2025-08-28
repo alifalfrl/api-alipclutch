1bcc24190de040c43acd8fa1b712c5a2b384ffdc
alifalfrl <alifalfarel10@gmail.com>
2025-08-28 07:33:48 +0700

    Update brat.js

--- api/brat.js
+++ api/brat.js
@@ -1,24 +1,26 @@
 module.exports = {
-  name: "Brat",
-  desc: "Brat text generator",
-  category: "Imagecreator",
-  path: "/imagecreator/brat?apikey=&text=",
-  async run(req, res) {
-    const { apikey, text } = req.query;
-    if (!global.apikey.includes(apikey))
-      return res.json({ status: false, error: 'Apikey invalid' });
-    if (!text)
-      return res.json({ status: false, error: 'Missing text' });
+name: "Brat",
+desc: "Brat text generator",
+category: "Imagecreator",
+path: "/imagecreator/bratv?apikey=&text=",
+async run(req, res) {
+const { apikey, text } = req.query;
+if (!global.apikey.includes(apikey))
+return res.json({ status: false, error: 'Apikey invalid' });
+if (!text)
+return res.json({ status: false, error: 'Missing text' });
 
-    const encodedText = encodeURIComponent(text);
-    const buffer = await getBuffer(
-      `https://api.zenzxz.my.id/maker/brat?text=${encodedText}`
-    );
+const encodedText = encodeURIComponent(text);  
+const buffer = await getBuffer(  
+  `https://api.zenzxz.my.id/maker/brat?text=${encodedText}&background=%23ffffff&color=%23000000&emojiStyle=apple`  
+);  
 
-    res.writeHead(200, {
-      'Content-Type': 'image/png',
-      'Content-Length': buffer.length,
-    });
-    res.end(buffer);
-  }
+res.writeHead(200, {  
+  'Content-Type': 'image/png',  
+  'Content-Length': buffer.length,  
+});  
+res.end(buffer);
+
 }
+}
+
